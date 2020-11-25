7319c4fa2aa625b44a4504e5b99138aa6ae5d0ed
Varun P M <varun.pm@tact.ai>
2020-11-26 01:01:53 +0530

    Updated to xcode 12 and replaced corresponding deprecated functions

--- Tests/KeychainTests.swift
+++ Tests/KeychainTests.swift
@@ -22,7 +22,7 @@ class KeychainTests: XCTestCase {
 	// MARK: - XCTestCase
 
 	override func tearDown() {
-		SAMKeychain.deletePasswordForService(testService, account: testAccount)
+		SAMKeychain.deletePassword(forService: testService, account: testAccount)
 		super.tearDown()
 	}
 
@@ -48,13 +48,13 @@ class KeychainTests: XCTestCase {
 
 		// Search for all accounts
 		let allQuery = SAMKeychainQuery()
-		var accounts = try! allQuery.fetchAll()
-		XCTAssertTrue(self.accounts(accounts, containsAccountWithName: testAccount), "Matching account was not returned")
+		var accounts = try! allQuery.fetchAll() as [[String: AnyObject]]
+		XCTAssertTrue(self.accounts(accounts: accounts, containsAccountWithName: testAccount), "Matching account was not returned")
 
 		// Check accounts for service
 		allQuery.service = testService
-		accounts = try! allQuery.fetchAll()
-		XCTAssertTrue(self.accounts(accounts, containsAccountWithName: testAccount), "Matching account was not returned")
+		accounts = try! allQuery.fetchAll() as [[String: AnyObject]]
+		XCTAssertTrue(self.accounts(accounts: accounts, containsAccountWithName: testAccount), "Matching account was not returned")
 
 		// Delete
 		let deleteQuery = SAMKeychainQuery()
@@ -68,7 +68,7 @@ class KeychainTests: XCTestCase {
 		newQuery.service = testService
 		newQuery.account = testAccount
 
-		let dictionary: [String: NSObject] = [
+		let dictionary: NSDictionary = [
 			"number": 42,
 			"string": "Hello World"
 		]
@@ -81,7 +81,7 @@ class KeychainTests: XCTestCase {
 		lookupQuery.account = testAccount
 		try! lookupQuery.fetch()
 
-		let readDictionary = lookupQuery.passwordObject as! [String: NSObject]
+		let readDictionary = lookupQuery.passwordObject as? NSDictionary
 		XCTAssertEqual(dictionary, readDictionary)
 	}
 
@@ -131,20 +131,20 @@ class KeychainTests: XCTestCase {
 		createQuery.service = testService
 		createQuery.account = testAccount
 		createQuery.password = testPassword
-		createQuery.synchronizationMode = .Yes
+		createQuery.synchronizationMode = .yes
 		try! createQuery.save()
 
 		let noFetchQuery = SAMKeychainQuery()
 		noFetchQuery.service = testService
 		noFetchQuery.account = testAccount
-	    noFetchQuery.synchronizationMode = .No
+	    noFetchQuery.synchronizationMode = .no
 		XCTAssertThrowsError(try noFetchQuery.fetch())
 		XCTAssertNotEqual(createQuery.password, noFetchQuery.password)
 
 		let anyFetchQuery = SAMKeychainQuery()
 		anyFetchQuery.service = testService
 		anyFetchQuery.account = testAccount
-		anyFetchQuery.synchronizationMode = .Any
+		anyFetchQuery.synchronizationMode = .any
 		try! anyFetchQuery.fetch()
 		XCTAssertEqual(createQuery.password, anyFetchQuery.password)
 	}
@@ -154,17 +154,17 @@ class KeychainTests: XCTestCase {
 		SAMKeychain.setPassword(testPassword, forService: testService, account: testAccount)
 
 		// Check password
-		XCTAssertEqual(testPassword, SAMKeychain.passwordForService(testService, account: testAccount))
+		XCTAssertEqual(testPassword, SAMKeychain.password(forService: testService, account: testAccount))
 
 		// Check all accounts
-		XCTAssertTrue(accounts(SAMKeychain.allAccounts(), containsAccountWithName: testAccount))
+		XCTAssertTrue(accounts(accounts: SAMKeychain.allAccounts() as [[String: AnyObject]], containsAccountWithName: testAccount))
 
 		// Check account
-		XCTAssertTrue(accounts(SAMKeychain.accountsForService(testService), containsAccountWithName: testAccount))
-
+		XCTAssertTrue(accounts(accounts: SAMKeychain.accounts(forService: testService) as [[String: AnyObject]], containsAccountWithName: testAccount))
+		
 		#if !os(OSX)
 			SAMKeychain.setAccessibilityType(kSecAttrAccessibleWhenUnlockedThisDeviceOnly)
-			XCTAssertEqual(String(kSecAttrAccessibleWhenUnlockedThisDeviceOnly), String(SAMKeychain.accessibilityType().takeRetainedValue()))
+			XCTAssertEqual(String(kSecAttrAccessibleWhenUnlockedThisDeviceOnly), String(describing: SAMKeychain.accessibilityType()!.takeRetainedValue()))
 		#endif
 	}
 
@@ -175,19 +175,19 @@ class KeychainTests: XCTestCase {
 		SAMKeychain.setPassword(testPassword, forService: testService, account: testAccount)
 
 		// Check all accounts
-		XCTAssertTrue(accounts(SAMKeychain.allAccounts(), containsAccountWithName: testAccount))
+		XCTAssertTrue(accounts(accounts: SAMKeychain.allAccounts() as [[String: AnyObject]], containsAccountWithName: testAccount))
 
 		// Check account
-		XCTAssertTrue(accounts(SAMKeychain.accountsForService(testService), containsAccountWithName: testAccount))
+		XCTAssertTrue(accounts(accounts: SAMKeychain.accounts(forService: testService) as [[String: AnyObject]], containsAccountWithName: testAccount))
 
-		SAMKeychain.setAccessibilityType(kSecAttrAccessibleAlwaysThisDeviceOnly)
+		SAMKeychain.setAccessibilityType(kSecAttrAccessibleWhenUnlockedThisDeviceOnly)
 		SAMKeychain.setPassword(testPassword, forService: testService, account: testAccount)
 
 		// Check all accounts
-		XCTAssertTrue(accounts(SAMKeychain.allAccounts(), containsAccountWithName: testAccount))
+		XCTAssertTrue(accounts(accounts: SAMKeychain.allAccounts() as [[String: AnyObject]], containsAccountWithName: testAccount))
 
 		// Check account
-		XCTAssertTrue(accounts(SAMKeychain.accountsForService(testService), containsAccountWithName: testAccount))
+		XCTAssertTrue(accounts(accounts: SAMKeychain.accounts(forService: testService) as [[String: AnyObject]], containsAccountWithName: testAccount))
 	}
 	
 
@@ -195,7 +195,7 @@ class KeychainTests: XCTestCase {
 
 	private func accounts(accounts: [[String: AnyObject]], containsAccountWithName name: String) -> Bool {
 		for account in accounts {
-			if let acct = account["acct"] as? String where acct == name {
+			if let acct = account["acct"] as? String, acct == name {
 				return true
 			}
 		}
