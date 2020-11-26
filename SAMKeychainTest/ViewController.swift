e94ddbad12523e0c532e0205866ba646d3458c05
Varun P M <varun.pm@tact.ai>
2020-11-26 17:31:27 +0530

    Added a test project too make test compilable due to missing dependency

---
+++ SAMKeychainTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SAMKeychainTest
+//
+//  Created by Varun P M on 26/11/20.
+//  Copyright © 2020 Sam Soffes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
